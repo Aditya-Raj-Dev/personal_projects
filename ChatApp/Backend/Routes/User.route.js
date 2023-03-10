eb9ef9136e5f2eb6e4a81ffd75688857b4c71156
Aditya Raj <aadiyadav56837492@gmail.com>
2023-03-10 09:14:55 +0530

    deleted

--- ChatApp/Backend/Routes/User.route.js
+++
@@ -1,9 +0,0 @@
-const {Router}=require("express")
-const { RegisterUser, SearchUser } = require("../Controller/UserController")
-
-const UserRouter=Router()
-
-UserRouter.post("/",RegisterUser)
-UserRouter.get("/",SearchUser)
-
-module.exports=UserRouter
